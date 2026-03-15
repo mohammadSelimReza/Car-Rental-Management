61c2b274f16a294f5c1c03f05537a43c0ea9ccce
mohammadSelimReza <srreza1999@gmail.com>
2026-03-15 12:39:28 +0600

    fix asgi and celery config

--- car_rental/asgi.py
+++ car_rental/asgi.py
@@ -1,11 +1,15 @@
 import os
+import django
 from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'car_rental.settings')
+django.setup()
+
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 import chat.routing
 from chat.middleware import TokenAuthMiddleware  # ← your middleware
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'car_rental.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
